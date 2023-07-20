364d67d8379b79d2fe6fc4207afb430772b595c2
Juan David Cardenas <110917866+jdcardenasBrock@users.noreply.github.com>
2023-07-19 21:06:00 -0500

    Mail Web

--- form_contact.php
+++ form_contact.php
@@ -1,3 +1,4 @@
+
 <section class="section-padding pb-0 section-bg quote_form_sec elements relative z-1" style="background-image: url(assets/images/bg/bg_7.jpg);">
     <div class="container relative z-1">
         <div class="row flex-lg-row-reverse align-items-end justify-content-between">
@@ -12,41 +13,41 @@
                             ENVÍENOS SU SOLICITUD O MENSAJE.
                         </p>
                     </div>
-                    <form class="wow fadeInUp" style="visibility: visible; animation-name: fadeInUp;">
+                    <form class="wow fadeInUp" style="visibility: visible; animation-name: fadeInUp;" action="sendmail.php">
                         <div class="row">
                             <div class="col-lg-6">
                                 <div class="form-group">
-                                    <input type="text" name="#" class="form-control form-control-custom style_2" placeholder="Nombre Completo" autocomplete="off">
+                                    <input type="text" name="name" id="name" class="form-control form-control-custom style_2" placeholder="Nombre Completo" autocomplete="off">
                                     <i class="fal fa-user"></i>
                                 </div>
                             </div>
                             <div class="col-lg-6">
                                 <div class="form-group">
-                                    <input type="email" name="#" class="form-control form-control-custom style_2" placeholder="Correo Electronico" autocomplete="off">
+                                    <input type="email" name="email" id="email" class="form-control form-control-custom style_2" placeholder="Correo Electronico" autocomplete="off">
                                     <i class="fal fa-envelope"></i>
                                 </div>
                             </div>
                             <div class="col-lg-6">
                                 <div class="form-group">
-                                    <input type="text" name="#" class="form-control form-control-custom style_2" placeholder="Pais y Ciudad" autocomplete="off">
+                                    <input type="text" name="country" id="country" class="form-control form-control-custom style_2" placeholder="Pais y Ciudad" autocomplete="off">
                                     <i class="fal fa-map"></i>
                                 </div>
                             </div>
                             <div class="col-lg-6">
                                 <div class="form-group">
-                                    <input type="text" name="#" class="form-control form-control-custom style_2" placeholder="Nombre Empresa" autocomplete="off">
+                                    <input type="text" name="company" id="company" class="form-control form-control-custom style_2" placeholder="Nombre Empresa" autocomplete="off">
                                     <i class="fal fa-building"></i>
                                 </div>
                             </div>
                             <div class="col-lg-6">
                                 <div class="form-group">
-                                    <input type="text" name="#" class="form-control form-control-custom style_2" placeholder="Telefono de Contacto" autocomplete="off">
+                                    <input type="text" name="phone" id="phone" class="form-control form-control-custom style_2" placeholder="Telefono de Contacto" autocomplete="off">
                                     <i class="fal fa-phone"></i>
                                 </div>
                             </div>
                             <div class="col-lg-6">
                                 <div class="form-group">
-                                    <select class="form-control form-control-custom style_2" name="#" style="color:#fff;">
+                                    <select class="form-control form-control-custom style_2" name="type_message" id="type_message" style="color:#fff;">
                                         <option selected="" >Tipo de Mensaje</option>
                                         <option value="option 1">Solicitud</option>
                                         <option value="option 2">Cotización</option>
@@ -58,7 +59,7 @@
                             </div>
                             <div class="col-lg-12">
                                 <div class="form-group">
-                                    <textarea class="form-control form-control-custom style_2" placeholder="Escriba su mensaje en esta sección." rows="5"></textarea>
+                                    <textarea name="message" id="message" class="form-control form-control-custom style_2" placeholder="Escriba su mensaje en esta sección." rows="5"></textarea>
                                 </div>
                             </div>
                             <div class="col-lg-12">
--- sendmail.php
+++ sendmail.php
@@ -1,139 +1,187 @@
-<?php
-    // Your email here
-    $email = 'example@example.com';
+<?php 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-    // Errors Object
-    $serverErrors = array();
+require 'assets/php_mailer/Exception.php';
+require 'assets/php_mailer/PHPMailer.php';
+require 'assets/php_mailer/SMTP.php'; 
 
-    // Errors strings
-    $name_error      = '*Invalid name (Only letters and white space allowed) <br>';
-    $email_error     = '*Invalid email format <br>';
-    $phone_error     = '*Invalid phone number <br>';
-    $subject_error   = '*Please choose the subject <br>';
-    $message_error   = '*Please write your message <br>';
 
-    // Status strings
-    $sent_message = 'Email Sent Successfully!';
-    $send_error_message = 'Error, please try again...';
+// Obtener los datos del formulario
 
-    // Email subject
-    $email_subject = 'New message from ' . "[$_SERVER[HTTP_HOST]]";
+$name= $_POST['name'];
+$email= $_POST['email'];
+$country= $_POST['country'];
+$company= $_POST['company'];
+$phone= $_POST['phone'];
+$type_message= $_POST['type_message'];
+$message= $_POST['message'];
 
-    $post_data = "";
+// Crea una nueva instancia de PHPMailer
+$mail = new PHPMailer(true);
 
-    $sender_email = '';
+try {
+    // Configurar el servidor SMTP de Hostinger
+    $mail->isSMTP();
+    $mail->Host = 'smtp.hostinger.com'; // Cambia esto por el servidor SMTP de Hostinger
+    $mail->SMTPAuth = true;
+    $mail->Username = 'notify_noreply@tmr.global'; // Cambia esto por tu dirección de correo
+    $mail->Password = 'Dademaro,123'; // Cambia esto por tu contraseña de correo
+    $mail->Port = 465; // Puerto SMTP de Hostinger
 
-    function secure ($var){
-        return stripslashes(htmlspecialchars($var));
-    }
-    $form_data = json_decode( file_get_contets( 'php://input' ), true );
+    // Dirección de correo del remitente
+    $mail->setFrom('notify_noreply@tmr.global', 'WebMail Tmr.global'); // Cambia esto por tu dirección de correo y tu nombre
 
-    foreach ($form_data as $key => $value){
-        $value = secure($value);
+    // Dirección de correo del destinatario
+    $mail->addAddress('juanchodavidcardenas@outlook.com'); // Cambia esto por la dirección del destinatario
 
-        switch ($key){
-            case 'name':
-                if( !preg_match("/^[a-zA-Z ]*$/", $value) || empty($value) ) {
-                    $serverErrors['errors'] .= $name_error;
-                }
-                else{
-                    $post_data .= "<strong>$key:</strong> $value <br>";
-                }
-                break;
-            case 'email':
-                if( !filter_var($value, FILTER_VALIDATE_EMAIL) ) {
-                    $serverErrors['errors'] .= $email_error;
-                }
-                else{
-                    $post_data .= "<strong>$key:</strong> $value <br>";
-                    $sender_email = $value;
-                }
-                break;
-            case 'phone':
-                if( empty($value) ){
-                    $serverErrors['errors'] .= $phone_error;
-                }
-                else{
-                    $post_data .= "<strong>$key:</strong> $value <br>";
-                }
-                break;
-            case 'subject':
-                if( empty($value) ){
-                    $serverErrors['errors'] .= $subject_error;
-                }
-                else{
-                    $post_data .= "<strong>$key:</strong> $value <br>";
-                }
-                break;
-            case 'message':
-                if( empty($value) ){
-                    $serverErrors['errors'] .= $message_error;
-                }
-                else{
-                    $post_data .= "<br><strong>$key:</strong> $value <br>";
-                }
-                break;
-            default: if( !empty($value) ) $post_data .= "<strong>$key:</strong> $value <br>";
-        }
-    }
+    // Contenido del correo
+    $mail->isHTML(true);
+    $mail->Subject = 'Formulario de contacto - Mensaje de ' . $nombre;
+    $mail->Body = "<p><b>Nombre:</b> $name</p><p><b>Email:</b> $email</p><p><b>Mensaje:</b><br>$message</p>";
 
-    // Set and return status ERROR if anny errors found
-    if( count($serverErrors) > 0 ) {
-        $serverErrors['status'] = 'error';
+    // Envío del correo
+    $mail->send();
+    echo 'Correo enviado exitosamente.';
+} catch (Exception $e) {
+    echo "Error al enviar el correo: {$mail->ErrorInfo}";
+}
 
-        // Return JSON Errors
-        echo json_encode($serverErrors, JSON_FORCE_OBJECT);
-        exit;
-    }
-    else{
-        /*
-        * If no errors prepare and send email
-        */
+    // // Your email here
+    // $email = 'notify_noreply@tmr.global';
 
-        if( !empty($sender_email) ){
-            $sender = "{$sender_email} <noreply@{$_SERVER['HTTP_HOST']}>";
-        }
-        else{
-            $sender = "{$email} <noreply@{$_SERVER['HTTP_HOST']}>";
-        }
+    // // Errors Object
+    // $serverErrors = array();
 
-        // Setting up header
-        $header  = 'MIME-Version: 1.0' . "\r\n";
-        $header .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-        $header .= 'From: '.$sender."\r\n".
-            'Reply-To: '.$sender."\r\n" .
-            'X-Mailer: PHP/' . phpversion();
+    // // Errors strings
+    // $name_error      = '*Invalid name (Only letters and white space allowed) <br>';
+    // $email_error     = '*Invalid email format <br>';
+    // $phone_error     = '*Invalid phone number <br>';
+    // $subject_error   = '*Please choose the subject <br>';
+    // $message_error   = '*Please write your message <br>';
 
-        //  $message = $post_data . "\n\n"."User IP: <a href='#'>User IP</a>" . $_SERVER['REMOTE_ADDR'] . "\n\n"  .date("H:i M.d.Y ")."\n";
+    // // Status strings
+    // $sent_message = 'Email Sent Successfully!';
+    // $send_error_message = 'Error, please try again...';
 
-        $message = '<html><body>';
-        $message .= '<p style="display: block; max-width: 550px; font-size: 16px">'. $post_data . '</p>';
-        $message .= '<br>';
-        $message .= '<a href="https://iplocation.com/?ip='.$_SERVER['REMOTE_ADDR'].'" style="text-decoration: underline; color: #27AE60"><strong style="color: #27AE60">VIEW USER LOCALIZATION</strong></a>';
-        $message .= '<p style="color: #999999; font-size: 14px"><strong>SENT:</strong> ' . date("H:i M.d.Y ") . '</p>';
-        $message .= '<p style="color: #999999; font-size: 11px; margin-top: 50px;"> Powered by <a href="#" style="color: #EB3B5B">Simple AJAX Contact Form</a></p>';
-        $message .= '</body></html>';
+    // // Email subject
+    // $email_subject = 'New message from ' . "[$_SERVER[HTTP_HOST]]";
 
-        // Verification before sending
-        $verify = mail($email,$email_subject,$message,$header);
+    // $post_data = "";
 
-        // Return Server Response
-        if ($verify == 'true'){
+    // $sender_email = '';
 
-            // Return Success Message
-            $serverErrors['status'] = 'success';
-            $serverErrors['message'] = $sent_message;
-            echo json_encode($serverErrors, JSON_FORCE_OBJECT);
+    // function secure ($var){
+    //     return stripslashes(htmlspecialchars($var));
+    // }
+    // $form_data = json_decode( file_get_contets( 'php://input' ), true );
 
-            exit;
-        }
-        else {
+    // foreach ($form_data as $key => $value){
+    //     $value = secure($value);
 
-            // Return Error Message
-            $serverErrors['status'] = 'error';
-            $serverErrors['message'] = $send_error_message;
-            echo json_encode($serverErrors, JSON_FORCE_OBJECT);
+    //     switch ($key){
+    //         case 'name':
+    //             if( !preg_match("/^[a-zA-Z ]*$/", $value) || empty($value) ) {
+    //                 $serverErrors['errors'] .= $name_error;
+    //             }
+    //             else{
+    //                 $post_data .= "<strong>$key:</strong> $value <br>";
+    //             }
+    //             break;
+    //         case 'email':
+    //             if( !filter_var($value, FILTER_VALIDATE_EMAIL) ) {
+    //                 $serverErrors['errors'] .= $email_error;
+    //             }
+    //             else{
+    //                 $post_data .= "<strong>$key:</strong> $value <br>";
+    //                 $sender_email = $value;
+    //             }
+    //             break;
+    //         case 'phone':
+    //             if( empty($value) ){
+    //                 $serverErrors['errors'] .= $phone_error;
+    //             }
+    //             else{
+    //                 $post_data .= "<strong>$key:</strong> $value <br>";
+    //             }
+    //             break;
+    //         case 'subject':
+    //             if( empty($value) ){
+    //                 $serverErrors['errors'] .= $subject_error;
+    //             }
+    //             else{
+    //                 $post_data .= "<strong>$key:</strong> $value <br>";
+    //             }
+    //             break;
+    //         case 'message':
+    //             if( empty($value) ){
+    //                 $serverErrors['errors'] .= $message_error;
+    //             }
+    //             else{
+    //                 $post_data .= "<br><strong>$key:</strong> $value <br>";
+    //             }
+    //             break;
+    //         default: if( !empty($value) ) $post_data .= "<strong>$key:</strong> $value <br>";
+    //     }
+    // }
 
-            exit;
-        }
-    }
+    // // Set and return status ERROR if anny errors found
+    // if( count($serverErrors) > 0 ) {
+    //     $serverErrors['status'] = 'error';
+
+    //     // Return JSON Errors
+    //     echo json_encode($serverErrors, JSON_FORCE_OBJECT);
+    //     exit;
+    // }
+    // else{
+    //     /*
+    //     * If no errors prepare and send email
+    //     */
+
+    //     if( !empty($sender_email) ){
+    //         $sender = "{$sender_email} <noreply@{$_SERVER['HTTP_HOST']}>";
+    //     }
+    //     else{
+    //         $sender = "{$email} <noreply@{$_SERVER['HTTP_HOST']}>";
+    //     }
+
+    //     // Setting up header
+    //     $header  = 'MIME-Version: 1.0' . "\r\n";
+    //     $header .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    //     $header .= 'From: '.$sender."\r\n".
+    //         'Reply-To: '.$sender."\r\n" .
+    //         'X-Mailer: PHP/' . phpversion();
+
+    //     //  $message = $post_data . "\n\n"."User IP: <a href='#'>User IP</a>" . $_SERVER['REMOTE_ADDR'] . "\n\n"  .date("H:i M.d.Y ")."\n";
+
+    //     $message = '<html><body>';
+    //     $message .= '<p style="display: block; max-width: 550px; font-size: 16px">'. $post_data . '</p>';
+    //     $message .= '<br>';
+    //     $message .= '<a href="https://iplocation.com/?ip='.$_SERVER['REMOTE_ADDR'].'" style="text-decoration: underline; color: #27AE60"><strong style="color: #27AE60">VIEW USER LOCALIZATION</strong></a>';
+    //     $message .= '<p style="color: #999999; font-size: 14px"><strong>SENT:</strong> ' . date("H:i M.d.Y ") . '</p>';
+    //     $message .= '<p style="color: #999999; font-size: 11px; margin-top: 50px;"> Powered by <a href="#" style="color: #EB3B5B">Simple AJAX Contact Form</a></p>';
+    //     $message .= '</body></html>';
+
+    //     // Verification before sending
+    //     $verify = mail($email,$email_subject,$message,$header);
+
+    //     // Return Server Response
+    //     if ($verify == 'true'){
+
+    //         // Return Success Message
+    //         $serverErrors['status'] = 'success';
+    //         $serverErrors['message'] = $sent_message;
+    //         echo json_encode($serverErrors, JSON_FORCE_OBJECT);
+
+    //         exit;
+    //     }
+    //     else {
+
+    //         // Return Error Message
+    //         $serverErrors['status'] = 'error';
+    //         $serverErrors['message'] = $send_error_message;
+    //         echo json_encode($serverErrors, JSON_FORCE_OBJECT);
+
+    //         exit;
+    //     }
+    // }
